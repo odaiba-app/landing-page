abea5f353c28471e78810c223b45e4b18e22304e
Frances Cheng <francesvcheng@gmail.com>
2020-07-07 23:49:28 -0700

    change navbar color on scroll

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
 // This is where it all goes :)
+
+//navbar
+window.onscroll = () => {
+  const nav = document.querySelector('#navbar');
+  if(this.scrollY <= 10) nav.className = 'bg-transparent fixed w-full p-6 m-auto z-50'; else nav.className = 'bg-white fixed w-full p-6 m-auto z-50';
+};
