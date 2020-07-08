77969592340f12cd0a7fc23daa219af0ba5a240b
Frances Cheng <francesvcheng@gmail.com>
2020-07-07 23:52:34 -0700

    added transition to navbar color change

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -3,5 +3,5 @@
 //navbar
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 10) nav.className = 'bg-transparent fixed w-full p-6 m-auto z-50'; else nav.className = 'bg-white fixed w-full p-6 m-auto z-50';
+  if(this.scrollY <= 10) nav.className = 'transition duration-300 bg-transparent fixed w-full p-6 m-auto z-50'; else nav.className = 'transition duration-300 bg-white fixed w-full p-6 m-auto z-50';
 };
