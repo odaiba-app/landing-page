4c662bb3aa6de9c221d069371f86d4e0e7ef0a0c
Frances Cheng <francesvcheng@gmail.com>
2020-07-09 03:16:06 -0700

    cleaned up top of mobile site

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -8,7 +8,7 @@ window.addEventListener('load', (event) => {
 //navbar
 window.onscroll = () => {
   const nav = document.querySelector('#navbar');
-  if(this.scrollY <= 10) nav.className = 'transition duration-300 bg-transparent fixed w-full p-6 m-auto z-50'; else nav.className = 'transition duration-300 bg-white fixed w-full p-6 m-auto z-50';
+  if(this.scrollY <= 10) nav.className = 'transition duration-300 bg-transparent fixed w-screen p-6 m-auto z-100'; else nav.className = 'transition duration-300 bg-white fixed w-screen p-6 m-auto z-100';
 };
 
 //toggle sidebar
