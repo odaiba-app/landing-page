5ebcce549d88b4308e322c1bf25cd7620242f0b4
Frances Cheng <francesvcheng@gmail.com>
2020-07-23 19:18:39 -0700

    menu items are in line horizontally

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -10,11 +10,3 @@ window.onscroll = () => {
   const nav = document.querySelector('#navbar');
   if(this.scrollY <= 10) nav.className = 'transition duration-300 bg-transparent fixed w-screen'; else nav.className = 'transition duration-300 bg-white fixed w-screen';
 };
-
-//toggle sidebar
-const sidebar = document.querySelector('.side-menu');
-const ham = document.querySelector('#hamburger');
-
-ham.addeventlistener('click', function() {
- sidebar.classList.toggle('.hidden')
-});
