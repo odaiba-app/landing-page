607a547ff419c5732ac2439ee1457616765285de
Frances Cheng <francesvcheng@gmail.com>
2020-07-31 16:55:06 -0700

    moved script tag to bottom of page

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -5,34 +5,31 @@ window.onscroll = () => {
   if(this.scrollY <= 10) nav.className = 'transition duration-300 bg-transparent fixed w-screen'; else nav.className = 'transition duration-300 bg-white fixed w-screen';
 };
 
-// all images inside the image modal content class
-const lightboxImages = document.querySelectorAll('.image-modal-content img');
+//modal
+const modalTriggers = document.querySelectorAll('.popup-trigger');
+console.log(modalTriggers);
+const modalCloseTrigger = document.querySelector('.popup-modal__close');
+console.log(modalCloseTrigger);
+const bodyBlackout = document.querySelector('.body-blackout');
 
-// dynamically selects all elements inside modal popup
-const modalElement = element =>
-  document.querySelector(`.image-modal-popup ${element}`);
+modalTriggers.forEach(trigger => {
+  trigger.addEventListener('click', () => {
+    console.log(trigger.dataset)
+    const { popupTrigger } = trigger.dataset
+    const popupModal = document.querySelector(`[data-popup-modal="${popupTrigger}"]`)
 
-const body = document.querySelector('body');
+    popupModal.classList.add('is--visible')
+    bodyBlackout.classList.add('is-blacked-out')
 
-// closes modal on clicking anywhere and adds overflow back
-document.addEventListener('click', () => {
-  body.style.overflow = 'auto';
-  modalPopup.style.display = 'none';
-});
+    popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+       popupModal.classList.remove('is--visible')
+       bodyBlackout.classList.remove('is-blacked-out')
+    });
 
-const modalPopup = document.querySelector('.image-modal-popup');
-
-// loops over each modal content img and adds click event functionality
-lightboxImages.forEach(img => {
-  const data = img.dataset;
-  img.addEventListener('click', e => {
-    body.style.overflow = 'hidden';
-    e.stopPropagation();
-    modalPopup.style.display = 'block';
-    modalElement('h1').innerHTML = data.title;
-    modalElement('p').innerHTML = data.description;
-    modalElement('a').href = data.url;
-    modalElement('.secondary-link').href = data.repo;
-    modalElement('img').src = img.src;
+    bodyBlackout.addEventListener('click', () => {
+      // TODO: Turn into a function to close modal
+      popupModal.classList.remove('is--visible')
+      bodyBlackout.classList.remove('is-blacked-out')
+    });
   });
 });
